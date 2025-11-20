4ffb55f237d7e60abf109e14073e38d49b053ed3
BAQ <tursymbekbakdaulet07@gmail.com>
2025-11-20 15:40:20 +0500

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,11 @@
 from app  import add
 from app import div
+from app import mul
 def test_add():
   assert add(2, 3) == 5
 
 def test_div():
   assert div(10, 5) == 2
+
+def test_mul():
+  assert mul(12, 10) == 121
